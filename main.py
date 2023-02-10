85e0d8adb234ec7934e0ba939a5b36d0d018b673
Zahra Ramadhani <123977443+zahradhn@users.noreply.github.com>
2023-02-10 17:15:47 +0700

    Test case

---
+++ main.py
@@ -0,0 +1,41 @@
+# Import module cashier.py
+import cashier
+
+# Test case
+trnsct_123 = Transaction()
+
+# Test Case 1
+print("Test Case 1")
+
+trnsct_123.add_item(["Ayam Goreng", 2, 20000.00])
+trnsct_123.add_item(["Pasta Gigi", 3, 15000.00])
+print("Item yang dibeli adalah: ", trnsct_123.items)
+print(" ")
+
+# Test Case 2
+print("Test Case 2")
+
+trnsct_123.delete_item("Pasta Gigi")
+print("Item yang dibeli setelah didelete : ", trnsct_123.items)
+print(" ")
+
+# Test Case 3
+print("Test Case 3")
+
+trnsct_123.reset_transaction()
+print(" ")
+
+# Test Case 4
+print("Test Case 4")
+
+trnsct_123.add_item(["Ayam goreng", 2, 20000.00])
+trnsct_123.add_item(["Pasta gigi", 3, 15000.00])
+trnsct_123.add_item(["Mainan mobil", 1, 200000.00])
+trnsct_123.add_item(["Indomie :D", 5, 3000.00])
+print(trnsct_123.items)
+print(" ")
+
+trnsct_123.hitung_total_price()
+print(" ")
+
+trnsct_123.check_order()
