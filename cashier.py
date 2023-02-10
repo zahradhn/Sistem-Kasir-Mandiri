93f5671336b96f25e92abbdebd42cdbde60e5c7e
Zahra Ramadhani <123977443+zahradhn@users.noreply.github.com>
2023-02-10 16:24:10 +0700

    Fixed all methods and TypeError by adding raise for invalid

--- cashier.py
+++ cashier.py
@@ -1,4 +1,5 @@
-# FINALLLLLLLL
+# Import table
+from tabulate import tabulate
 
 # Class
 class Transaction:
@@ -15,36 +16,56 @@ class Transaction:
     
     def add_item(self, item):
         #Menambahkan transaksi ke dalam list "items" yang berisi nama item [0], jumlah item [1], dan harga per item [2].
+
+        if type(item[0]) != str:
+           raise TypeError("Format nama item yang anda masukkan salah (gunakan tanda petik sebelum dan sesudah nama item).")
+        if type(item[1]) != int:
+           raise TypeError("Format jumlah item yang anda masukkan salah (misalnya: 12).")
+        if type(item[2]) != float:
+           raise TypeError("Format harga item yang anda masukkan salah (misalnya: 25000.00).")
+    
         self.items.append(item)
     
     def update_item_name(self, item_name, new_item):
         #Mengganti nama item yang sudah ditambahkan ke dalam list "items"
         for item in self.items:
-            if item == item_name:
+            if item[0] == item_name:
                 item[0] = new_item
+
+        if type(new_item) != str:
+            raise TypeError("Format nama item yang anda masukkan salah (gunakan tanda petik sebelum dan sesudah nama item).")
     
     def update_item_quantity(self, item_name, new_quantity):
         #blabla
         for item in self.items:
             if item[0] == item_name:
                 item[1] = new_quantity
+
+        if type(new_quantity) != int:
+            raise TypeError("Format jumlah item yang anda masukkan salah (misalnya: 12).")
     
     def update_item_price(self, item_name, new_price):
         #blabla
         for item in self.items:
-            if item[0] == self.item_name:
-                item[3] = new_price
+            if item[0] == item_name:
+                item[2] = new_price
     
+        if type(new_price) != float:
+           raise TypeError("Format harga item yang anda masukkan salah (misalnya: 25000.00).")
+
     def delete_item(self, item_name):
         #Delete item
         for item in self.items:
             if item[0] == item_name:
                 self.items.remove(item)
+
+        if type(item_name) != str:
+            raise TypeError("Format nama item yang anda masukkan salah (gunakan tanda petik sebelum dan sesudah nama item).")
     
     def reset_transaction(self):
         #Reset transaksi
         self.items = []
-        print("Cart is empty!")
+        print("Keranjang belanja telah dikosongkan!")
     
     def hitung_total_price(self):
         
@@ -63,7 +84,11 @@ class Transaction:
             self.discount = 0.08
         elif self.total_price > 200_000:
             self.discount = 0.05
-            
+
+        print("Total harga sebelum diskon: ", self.total_price)
+        print("Diskon yang didapatkan: ", self.total_price * self.discount)
+        print("Total harga setelah diskon: ", self.total_price - (self.total_price * self.discount))
+
         return self.total_price
     
     def check_order(self):
