dca6d4a3c7bbf0d99fb91e8dad3681c8e83f9ab2
Zahra Ramadhani <123977443+zahradhn@users.noreply.github.com>
2023-02-13 07:29:09 +0700

    Update and rename main.py to test-case.py

--- test-case.py
+++ test-case.py
@@ -10,20 +10,17 @@ print("Test Case 1")
 trnsct_123.add_item(["Ayam Goreng", 2, 20000.00])
 trnsct_123.add_item(["Pasta Gigi", 3, 15000.00])
 print("Item yang dibeli adalah: ", trnsct_123.items)
-print(" ")
 
 # Test Case 2
 print("Test Case 2")
 
 trnsct_123.delete_item("Pasta Gigi")
-print("Item yang dibeli setelah didelete : ", trnsct_123.items)
-print(" ")
+print("Item yang dibeli adalah: ", trnsct_123.items)
 
 # Test Case 3
 print("Test Case 3")
 
 trnsct_123.reset_transaction()
-print(" ")
 
 # Test Case 4
 print("Test Case 4")
@@ -31,7 +28,7 @@ print("Test Case 4")
 trnsct_123.add_item(["Ayam goreng", 2, 20000.00])
 trnsct_123.add_item(["Pasta gigi", 3, 15000.00])
 trnsct_123.add_item(["Mainan mobil", 1, 200000.00])
-trnsct_123.add_item(["Indomie :D", 5, 3000.00])
+trnsct_123.add_item(["Mie instan", 5, 3000.00])
 print(trnsct_123.items)
 print(" ")
 
